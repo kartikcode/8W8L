e39532fa96f7af9f6411594f1819b023de0d60b7
Kartikeya <gkartik@iitk.ac.in>
2020-05-04 09:17:05 +0530

    Better versions

--- python/cat.py
+++ python/cat.py
@@ -14,7 +14,7 @@ def validation(file_name):
     
 my_parser = argparse.ArgumentParser(description="Displaying the contents of a text file.")
 my_parser.add_argument( 'filename',
-                        type=argparse.FileType('r'),
+                        type=str,
                         nargs='*',
                         help='If specified, use the file instead of searching '
                         )
@@ -33,25 +33,30 @@ args = my_parser.parse_args()
 
 if (args.redirect):
     source = args.filename[0]
+    #print(source)
     destination = args.filename[1]
-    with open(str(destination.name), 'w') as out_file:
-        for line in source:
-            out_file.write(line)
+    #print(destination)
+    with open(destination, 'w') as out_file:
+        with open(source,'r') as source:
+            for line in source:
+                #print(line)
+                out_file.write(line)
 
     
 
 else:
-    for file in args.filename:
+    for file_name in args.filename:
         ##validation(file)
-        print("File name: {}".format(file.name))
-        if (args.number):
-            number=1
-            for line in file:
-                print(number, " ", line)
-                number = number + 1
-        else:
-            for line in file:
-                print(line)
+        print("File name: {}".format(file_name))
+        with open(file_name,'r') as file:
+            if (args.number):
+                number=1
+                for line in file:
+                    print(number, " ", line)
+                    number = number + 1
+            else:
+                for line in file:
+                    print(line)
         print("####################")
 
 
